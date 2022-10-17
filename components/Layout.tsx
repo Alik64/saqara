d793190a748e5e44f8a2599d0c77829f2fc2bed8
Alik64 <albert.barsamov@gmail.com>
2022-10-17 13:39:29 +0200

    styling header

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
+import Image from "next/image";
 import React from "react";
-
+import pokeball from "../assets/images/Pokeballpokeball.svg";
 type LayoutProps = {
   content?: string;
   title: string;
@@ -15,7 +16,9 @@ const Layout: React.FC<LayoutProps> = ({ children, content, title }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="header">Header</div>
+      <div className="flex justify-center py-4 bg-blue-500">
+        <Image src={pokeball} height={50} width={50} />
+      </div>
       <div>{children}</div>
     </>
   );
