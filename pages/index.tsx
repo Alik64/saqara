166d4c45a28d2ae7895d8f226f1cf87b9eef5b0a
Alik64 <albert.barsamov@gmail.com>
2022-10-17 13:39:16 +0200

    Styling inputs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import { useCallback, useMemo, useState } from "react";
 import { GetServerSideProps } from "next";
 import { useQuery } from "react-query";
-import { useRouter } from "next/router";
 
 import { Pokemon, PokemonData } from "../interfaces";
-
+import pokeball from "../assets/images/pokeball.png";
 import styles from "../styles/Home.module.css";
 import PokemonList from "../components/PokemonList";
 import Layout from "../components/Layout";
+import Image from "next/image";
 
 type InitialPokemonData = {
   count: number;
@@ -44,8 +44,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
 const Home: React.FC<HomeProps> = ({ initialPokemonData }) => {
   const [page, setPage] = useState<number>(0);
 
-  console.log("router", page);
-
   const { data: pokemons, isLoading } = useQuery(["pokemons", page], () =>
     getPokemons(page)
   );
@@ -73,15 +71,21 @@ const Home: React.FC<HomeProps> = ({ initialPokemonData }) => {
 
   return (
     <Layout title="Pokemons">
-      <div>
+      <div className="flex justify-center flex-row my-2">
+        <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 rounded-l-md border border-r-0 border-gray-300 ml-2">
+          <Image src={pokeball} alt="pokeball" width={30} height={30} />
+        </span>
+
         <input
           type="text"
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
-          className={styles.search}
+          className="rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5 mr-2"
           placeholder="Type here to search"
         />
+
         <select
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 mr-3"
           name="generations"
           id="generations"
           onChange={(e) => setPage(Number(e.target.value))}
@@ -104,10 +108,22 @@ const Home: React.FC<HomeProps> = ({ initialPokemonData }) => {
           <PokemonList pokemons={cashedFilteredPokemons} />
         )}
       </main>
-      <section>
-        {page > 0 && <button onClick={prevPageHandler}>Previous page</button>}
+      <section className="flex justify-center my-4">
+        {page > 0 && (
+          <button
+            className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mr-4 w-24"
+            onClick={prevPageHandler}
+          >
+            Previous
+          </button>
+        )}
         {pokemons?.count && page < pokemons?.count - 20 && (
-          <button onClick={nextPageHandler}>Next page</button>
+          <button
+            className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded w-24"
+            onClick={nextPageHandler}
+          >
+            Next
+          </button>
         )}
       </section>
     </Layout>
